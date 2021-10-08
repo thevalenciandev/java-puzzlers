17799f92a3a54e6502016885e9da6746327592ea
thevalenciandev <thevalenciandev@gmail.com>
2021-10-08 13:07:38 +0100

    Performance optimisation for modulo op.

--- src/main/java/com/thevalenciandev/jpuzzlers/ch1/Oddity.java
+++ src/main/java/com/thevalenciandev/jpuzzlers/ch1/Oddity.java
@@ -3,6 +3,6 @@ package com.thevalenciandev.jpuzzlers.ch1;
 public class Oddity {
 
     public static boolean isOdd(int i) {
-        return Math.abs(i % 2) == 1;
+        return (i & 1) != 0;
     }
 }
