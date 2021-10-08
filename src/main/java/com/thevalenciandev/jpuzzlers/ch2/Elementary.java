c3150883b8844f5a4c033f8b097320def0a277f9
thevalenciandev <thevalenciandev@gmail.com>
2021-10-08 13:33:11 +0100

    Chapter 2: (El)ementary

---
+++ src/main/java/com/thevalenciandev/jpuzzlers/ch2/Elementary.java
@@ -0,0 +1,8 @@
+package com.thevalenciandev.jpuzzlers.ch2;
+
+public class Elementary {
+
+    public static void main(String[] args) {
+        System.out.println(12345 + 5432l); // 17777: Avoid using l (el), to mark a Long, it's confusing!
+    }
+}
